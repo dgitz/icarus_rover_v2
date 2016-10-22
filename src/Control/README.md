fbf1f8bdb3dee57d04ec0c1c9e92068e4185d049
robot <davidgitz@gmail.com>
2016-10-22 18:41:34 -0500

    RViz and V-Rep are now in same reference system for Arm.

--- src/Control/README.md
+++ src/Control/README.md
@@ -7,3 +7,7 @@ Provides a master node
 
 B. command_node
 
+
+C. Arm Controller
+Purpose:
+Read in the desired pose of the gripper of each Arm, compute a path for the Arm and generate the target joint angles.
--- src/Control/armcontroller_node.cpp
+++ src/Control/armcontroller_node.cpp
@@ -1,46 +1,10 @@
-#include "tfhelper_node.h"
+#include "armcontroller_node.h"
 //Start Template Code: Firmware Definition
-#define TFHELPERNODE_MAJOR_RELEASE 1
-#define TFHELPERNODE_MINOR_RELEASE 1
-#define TFHELPERNODE_BUILD_NUMBER 0
+#define ARMCONTROLLERNODE_MAJOR_RELEASE 1
+#define ARMCONTROLLERNODE_MINOR_RELEASE 1
+#define ARMCONTROLLERNODE_BUILD_NUMBER 0
 //End Template Code: Firmware Definition
 //Start User Code: Functions
-void joint_callback(const std_msgs::Float32::ConstPtr& msg,const std::string &topic)
-{
-	joint_state.header.stamp = ros::Time::now();
-	for(int i = 0; i < joint_state.name.size();i++)
-	{
-		std::size_t found = topic.find(joint_state.name[i]);
-		if(found != std::string::npos)
-		{
-			joint_state.position[i] = msg->data*3.14159/180.0;
-
-
-			break;
-		}
-	}
-}
-bool publish_transforms()
-{
-	joint_state.header.stamp = ros::Time::now();
-	joint_pub.publish(joint_state);
-	static tf::TransformBroadcaster br;
-	geometry_msgs::TransformStamped odom_trans;
-    odom_trans.header.frame_id = "odom";
-    odom_trans.child_frame_id = "base_link";
-    odom_trans.header.stamp = ros::Time::now();
-    odom_trans.transform.translation.x = cos(0)*2;
-    odom_trans.transform.translation.y = sin(0)*2;
-    odom_trans.transform.translation.z = .7;
-    odom_trans.transform.rotation = tf::createQuaternionMsgFromYaw(0+M_PI/2);
-    br.sendTransform(odom_trans);
-
-	return true;
-}
-void device_Callback(const icarus_rover_v2::device::ConstPtr& msg)
-{
-}
-
 bool run_fastrate_code()
 {
 	//logger->log_debug("Running fast rate code.");
@@ -49,7 +13,6 @@ bool run_fastrate_code()
 bool run_mediumrate_code()
 {
 	//logger->log_debug("Running medium rate code.");
-	publish_transforms();
 	diagnostic_status.Diagnostic_Type = SOFTWARE;
 	diagnostic_status.Level = INFO;
 	diagnostic_status.Diagnostic_Message = NOERROR;
@@ -86,9 +49,9 @@ bool run_veryslowrate_code()
 	fw.Generic_Node_Name = "tfhelper_node";
 	fw.Node_Name = node_name;
 	fw.Description = "Latest Rev: 22-Oct-2016";
-	fw.Major_Release = TFHELPERNODE_MAJOR_RELEASE;
-	fw.Minor_Release = TFHELPERNODE_MINOR_RELEASE;
-	fw.Build_Number = TFHELPERNODE_BUILD_NUMBER;
+	fw.Major_Release = ARMCONTROLLERNODE_MAJOR_RELEASE;
+	fw.Minor_Release = ARMCONTROLLERNODE_MINOR_RELEASE;
+	fw.Build_Number = ARMCONTROLLERNODE_BUILD_NUMBER;
 	firmware_pub.publish(fw);
 	return true;
 }
@@ -98,7 +61,7 @@ bool run_veryslowrate_code()
 int main(int argc, char **argv)
 {
  
-	node_name = "tfhelper_node";
+	node_name = "armcontroller_node";
 
 
     ros::init(argc, argv, node_name);
@@ -177,7 +140,7 @@ bool initialize(ros::NodeHandle nh)
 	diagnostic_status.Node_Name = node_name;
 	diagnostic_status.System = ROVER;
 	diagnostic_status.SubSystem = ROBOT_CONTROLLER;
-	diagnostic_status.Component = DYNAMICS_NODE;
+	diagnostic_status.Component = CONTROLLER_NODE;
 
 	diagnostic_status.Diagnostic_Type = NOERROR;
 	diagnostic_status.Level = INFO;
@@ -223,39 +186,6 @@ bool initialize(ros::NodeHandle nh)
     //End Template Code: Initialization and Parameters
 
     //Start User Code: Initialization and Parameters
-    joint_pub = nh.advertise<sensor_msgs::JointState>("joint_states", 1);
-
-    //THIS SHOULD BE PULLED FROM THE ROBOTMODEL.xml file.
-    ros::master::V_TopicInfo master_topics;
-    ros::master::getTopics(master_topics);
-    std::vector<std::string> leftarmjoint_topics;
-    for (ros::master::V_TopicInfo::iterator it = master_topics.begin() ; it != master_topics.end(); it++)
-    {
-    	const ros::master::TopicInfo& info = *it;
-    	std::size_t found = info.name.find("leftarm");
-
-    	if(found != std::string::npos)
-    	{
-    		leftarmjoint_topics.push_back(info.name);
-    	}
-
-    }
-    ros::Subscriber * leftarm_subs;
-    leftarm_subs = new ros::Subscriber[leftarmjoint_topics.size()];
-    for(int i = 0; i < leftarmjoint_topics.size();i++)
-    {
-    	char tempstr[150];
-    	sprintf(tempstr,"Subscribing to Left Arm topic: %s",leftarmjoint_topics.at(i).c_str());
-    	logger->log_info(tempstr);
-    	leftarm_subs[i] = nh.subscribe<std_msgs::Float32>(leftarmjoint_topics.at(i),1000,boost::bind(joint_callback,_1,leftarmjoint_topics.at(i)));
-    }
-    joint_state.name.resize(5);
-    joint_state.position.resize(5);
-    joint_state.name[0] ="leftarm_baserotate";
-    joint_state.name[1] ="leftarm_armrotate";
-    joint_state.name[2] ="leftarm_forearmrotate";
-    joint_state.name[3] ="leftarm_gripperpivot";
-    joint_state.name[4] ="leftarm_gripperrotate";
     //Finish User Code: Initialization and Parameters
 
     //Start Template Code: Final Initialization.
