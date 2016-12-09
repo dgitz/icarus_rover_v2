88ef3d9889242ed3758b43306a2803c9e8391d29
robot <davidgitz@gmail.com>
2016-12-08 22:13:07 -0600

    More work on integrating tensorflow.

--- src/Vision/imageclassifier_node.py
+++ src/Vision/imageclassifier_node.py
@@ -3,6 +3,7 @@
 import rospy
 import tensorflow
 from std_msgs.msg import String
+import time
 import pdb
 def talker():
     pub = rospy.Publisher('chatter', String, queue_size=10)
@@ -11,10 +12,26 @@ def talker():
                    in tensorflow.gfile.GFile(
                        "/home/robot/other_packages/tensorflow/tensorflow/examples/label_image/data/imagenet_comp_graph_label_strings.txt")]
                    
-    for i in range(0,len(label_lines)):
-        print label_lines[i]
-    rate = rospy.Rate(10) # 10hz
+    with tensorflow.gfile.FastGFile("/home/robot/other_packages/tensorflow/tensorflow/examples/label_image/data/tensorflow_inception_graph.pb", 'rb') as f:
+        graph_def = tensorflow.GraphDef()
+        graph_def.ParseFromString(f.read())
+        _ = tensorflow.import_graph_def(graph_def, name='')
+    with tensorflow.Session() as sess:
+        softmax_tensor = sess.graph.get_tensor_by_name('softmax:0')
+    rate = rospy.Rate(100) # 10hz
     while not rospy.is_shutdown():
+        image_data = tensorflow.gfile.FastGFile("/home/robot/config/targets/outlet/outlet1.jpg", 'rb').read()
+        
+        start = time.time()
+        
+        predictions = sess.run(softmax_tensor, \
+           {'DecodeJpeg/contents:0': image_data})
+        top_k = predictions[0].argsort()[-len(predictions[0]):][::-1]
+        end = time.time()
+        rospy.loginfo("Duration: %.5f",end-start)
+        for j in range(0,5):
+             rospy.loginfo("Label: %s score: %.5f",label_lines[top_k[j]],predictions[0][top_k[j]])
+                
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
         pub.publish(hello_str)
