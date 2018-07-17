a0d5563430431d0ab16e2fe01d902a61ee252111
robot <davidgitz@gmail.com>
2018-07-16 22:36:29 -0500

    Major work complete.  Need to run on ControlModule though.

---
+++ src/Compute/README.md
@@ -0,0 +1,22 @@
+Author: David Gitz
+Task: Compute
+Support Programs
+A. Test LoopTimer
+
+Usage: This test program can run on any Linux device and is used to analyze how fast a loop can iterate, using the following methods:
+ - 
+  >>cd ~/catkin_ws/src/icarus_rover_v2/src/Compute/unit_tests/
+  >>g++ test_looptimer.cpp -o test_looptimer
+
+Results:
+Ubuntu x86/64 PC (Linux dgitzrosmaster 3.19.0-80-generic #88~14.04.1-Ubuntu SMP Fri Jan 13 14:54:07 UTC 2017 x86_64 x86_64 x86_64 GNU/Linux)
+Running Simple Loop Iterator.
+Time delta: 19.9942 sec.
+Running Pointer Loop Iterator.
+Time delta: 14.576 sec.
+Time Improvement: 27.0991%
+
+
+
+
+
---
+++ src/Compute/unit_tests/test_looptimer.cpp
@@ -0,0 +1,93 @@
+#include <iostream>
+#include <unistd.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#define LOOP_COUNT 100
+
+using namespace std;
+double compute_timediff(struct timeval a,struct timeval b);
+bool run_loopsimple()
+{
+
+	int a[LOOP_COUNT][LOOP_COUNT];
+	int b[LOOP_COUNT][LOOP_COUNT];
+	int c[LOOP_COUNT][LOOP_COUNT];
+
+	for(int i = 0; i < LOOP_COUNT; i++)
+	{
+
+		for(int j = 0; j < LOOP_COUNT;j++)
+		{
+
+			c[i][j] = 0;
+			//a[i][j] = i;
+			//b[i][j] = j;
+			for(int k = 0; k < LOOP_COUNT;k++)
+			{
+				c[i][j] += a[i][k] + b[k][j];
+			}
+
+		}
+
+	}
+
+	return true;
+}
+bool run_looppointer()
+{
+	int a[LOOP_COUNT][LOOP_COUNT];
+		int b[LOOP_COUNT][LOOP_COUNT];
+		int c[LOOP_COUNT][LOOP_COUNT];
+		for(int i = 0; i < LOOP_COUNT;i++)
+		{
+			int *cptr = c[i];
+			int *bptr = b[0];
+			for(int j = 0; j < LOOP_COUNT; j++)
+			{
+				int *aptr = a[i];
+				*cptr = (*aptr++) * (*bptr++);
+				for(int k = 1; k < LOOP_COUNT;k++)
+				{
+					*cptr += (*aptr++) * b[k][j];
+				}
+				cptr++;
+			}
+		}
+		return true;
+}
+int main (void)
+{
+	struct timeval start;
+	struct timeval now;
+	double time_loopsimple,time_looppointer;
+	usleep(100000);
+	cout << "Running Simple Loop Iterator." << endl;
+	gettimeofday(&start,NULL);
+	for(int i = 0; i < LOOP_COUNT*10; i++)
+	{
+		run_loopsimple();
+	}
+
+	gettimeofday(&now,NULL);
+	time_loopsimple = compute_timediff(start,now);
+	cout << "Time delta: " << time_loopsimple << " sec." << std::endl;
+	cout << "Running Pointer Loop Iterator." << endl;
+	gettimeofday(&start,NULL);
+	for(int i = 0; i < LOOP_COUNT*10; i++)
+	{
+		run_looppointer();
+	}
+	gettimeofday(&now,NULL);
+	time_looppointer = compute_timediff(start,now);
+	cout << "Time delta: " << time_looppointer << " sec." << std::endl;
+	cout << "Time Improvement: " << 100.0*(time_loopsimple-time_looppointer)/(time_loopsimple) << "%" << std::endl;
+	return 0;
+}
+double compute_timediff(struct timeval a,struct timeval b)
+{
+	double t1 = (double)a.tv_sec + (double)a.tv_usec/1000000.0;
+	double t2 = (double)b.tv_sec + (double)b.tv_usec/1000000.0;
+	return t2-t1;
+}
